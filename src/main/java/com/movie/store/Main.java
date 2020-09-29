5effeb07338d0243728baed845b34833be6bf355
vladd <vladdyachkin11@gmail.com>
2020-09-29 17:48:35 +0300

    configure logs file

--- src/main/java/com/movie/store/Main.java
+++ src/main/java/com/movie/store/Main.java
@@ -3,9 +3,11 @@ package com.movie.store;
 import com.movie.store.lib.Injector;
 import com.movie.store.model.Movie;
 import com.movie.store.service.MovieService;
+import org.apache.log4j.Logger;
 
 public class Main {
     private static Injector injector = Injector.getInstance("com.movie.store");
+    private static final Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
