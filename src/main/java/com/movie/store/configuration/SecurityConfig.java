66f3cb28aa6406c79da1777d36c73aeff3319865
vladd <vladdyachkin11@gmail.com>
2020-10-28 12:36:33 +0300

    before checkout

---
+++ src/main/java/com/movie/store/configuration/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.movie.store.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("12345" +
+                "").roles("USER");
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
---
+++ src/main/java/com/movie/store/configuration/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.movie.store.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
