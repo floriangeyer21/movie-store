9ea2f10724e8bf5a1738672536cc199c1cd0a673
vladd <vladdyachkin11@gmail.com>
2020-10-31 11:23:50 +0300

    remove empty line from exception handler

--- src/main/java/com/movie/store/controllers/CustomGlobalExceptionHandler.java
+++ src/main/java/com/movie/store/controllers/CustomGlobalExceptionHandler.java
@@ -26,15 +26,12 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", new Date());
         body.put("status", status.value());
-
         List<String> errors = ex.getBindingResult()
                 .getAllErrors()
                 .stream()
                 .map(ObjectError::getDefaultMessage)
                 .collect(Collectors.toList());
-
         body.put("errors", errors);
-
         return new ResponseEntity<>(body, headers, status);
 
     }
